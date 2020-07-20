cc9ab6ffe8d051b3c0f52a42e6ff72d04ba0604d
TGion <tobias@gion.io>
2020-07-20 12:48:44 +0200

    Project created

---
+++ src/cpp-dec2bin.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cpp-dec2bin.cpp
+// Author      : Tobias Gion
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "dec2bin" << endl; // prints dec2bin
+	return 0;
+}
